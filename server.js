58b11b0b1d81b30d766667feee6dff2f4e41d6f9
Rahul <rahul.gdr29@gmail.com>
2022-09-11 23:05:29 +0530

    added style.css and home.ejs | added access of public path in server.js using path module

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express=require('express');
 const dotenv=require('dotenv');
 const morgan=require('morgan');
+const path = require('path')
 
 const app=express();
 
 app.use(morgan('tiny'));
+app.set("view engine","ejs");
 
 dotenv.config({path:'config.env'});
 const PORT=process.env.PORT;
 
 app.get('/',(req,res)=>{
-    res.send("CRUD Application");
+    res.render('home');
 });
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT,()=>{ console.log(`Server is listening of Port ${PORT}`); });
